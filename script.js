66f6187502a609ea345fe1391a40d935758f54e4
RaivenGit <johnraivenolazo@gmail.com>
2023-06-25 00:06:57 +0800

    Update Files

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ const observer = new IntersectionObserver((entries) => {
      });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
